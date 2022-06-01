7bdbc4ec7b21cb401b8ae0625d232cba6435c3db
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2022-06-01 17:34:27 -0500

    Reformat code + movement backwords

--- eso-rpg/__main__.py
+++ eso-rpg/__main__.py
@@ -1,24 +1,29 @@
 import re
 import levels
 
+
 def get_facing(level, coords, direction):
     if direction == 0:
-        return level[coords[0]-1][coords[1]]
+        return level[coords[0] - 1][coords[1]]
     elif direction == 1:
-        return level[coords[0]][coords[1]+1]
+        return level[coords[0]][coords[1] + 1]
     elif direction == 2:
-        return level[coords[0]+1][coords[1]]
+        return level[coords[0] + 1][coords[1]]
     elif direction == 3:
-        return level[coords[0]][coords[1]-1]
+        return level[coords[0]][coords[1] - 1]
+
+
 def move_forward(amount, coords, direction):
     if direction == 0:
-        return [coords[0]-amount, coords[1]]
+        return [coords[0] - amount, coords[1]]
     elif direction == 1:
-        return [coords[0], coords[1]+amount]
+        return [coords[0], coords[1] + amount]
     elif direction == 2:
-        return [coords[0]+amount, coords[1]]
+        return [coords[0] + amount, coords[1]]
     elif direction == 3:
-        return [coords[0], coords[1]-amount]
+        return [coords[0], coords[1] - amount]
+
+
 def main():
     try:
         with open('program.esorpg', 'r') as f:
@@ -34,19 +39,22 @@ def main():
             player_coordinates = [y, x.index('@')]
     # End provided code
     level[player_coordinates[0]][player_coordinates[1]] = '0'
-    player_facing = 0 # 0N, 1E, 2S, 3W
+    player_facing = 0  # 0N, 1E, 2S, 3W
     dialogue = False
-    vars_vals = {"e": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0], "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 2 else [0], "d": [1] if dialouge else [0], "i": [get_facing(level, player_coordinates, player_facing)]}
+    vars_vals = {
+        "e": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0],
+        "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 2 else [0],
+        "d": [1] if dialouge else [0], "i": [get_facing(level, player_coordinates, player_facing)]}
     for line in lines:
-        if_stack = 0 # +1 for every (, -1 for every ). Should be 0 by the end of the line.
+        if_stack = 0  # +1 for every (, -1 for every ). Should be 0 by the end of the line.
         if_not_stack = 0
-        skip_stack = 0 # While >0, skip the character.
-        string_builder = [] # When in string mode, each new character is added here.
+        skip_stack = 0  # While >0, skip the character.
+        string_builder = []  # When in string mode, each new character is added here.
         string_mode = False
         skip_until_end_if = False
         skip_until_end_not = False
-        action_on_string_finish = None # Something like "print" or "set"
-        variable_to_set = None # Used for when setting a variable for action_on_string_finish
+        action_on_string_finish = None  # Something like "print" or "set"
+        variable_to_set = None  # Used for when setting a variable for action_on_string_finish
         for iteration in range(len(line)):
             if skip_stack > 0:
                 skip_stack -= 1
@@ -75,17 +83,19 @@ def main():
             elif char == '(':
                 skip_stack += 1
                 try:
-                    if vars_vals[line[iteration+1]][0] == 1:
+                    if vars_vals[line[iteration + 1]][0] == 1:
                         if_stack += 1
                 except KeyError:
-                    return print("!!! eso rpg error !!!\nif statement requesting a variable or value that doesn't exist yet")
+                    return print(
+                        "!!! eso rpg error !!!\nif statement requesting a variable or value that doesn't exist yet")
             elif char == '[':
                 skip_stack += 1
                 try:
-                    if vars_vals[line[iteration+1]][0] != 1:
+                    if vars_vals[line[iteration + 1]][0] != 1:
                         if_not_stack += 1
                 except KeyError:
-                    return print("!!! eso rpg error !!!\nif not statement requesting a variable or value that doesn't exist yet")
+                    return print(
+                        "!!! eso rpg error !!!\nif not statement requesting a variable or value that doesn't exist yet")
             elif char == '>':
                 action_on_string_finish = "print"
             elif char == "'":
@@ -98,23 +108,50 @@ def main():
                 if_not_stack -= 1
             elif char == 'w':
                 player_coordinates[0] += 1
-                vars_vals = {"e": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0], "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 2 else [0], "i": [get_facing(level, player_coordinates, player_facing)]}
+                vars_vals = {"e": [1] if len(
+                    levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0],
+                             "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)][
+                                                 'actions']) >= 2 else [0],
+                             "i": [get_facing(level, player_coordinates, player_facing)]}
             elif char == 'a':
                 player_coordinates[1] -= 1
-                vars_vals = {"e": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0], "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 2 else [0], "i": [get_facing(level, player_coordinates, player_facing)]}
+                vars_vals = {"e": [1] if len(
+                    levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0],
+                             "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)][
+                                                 'actions']) >= 2 else [0],
+                             "i": [get_facing(level, player_coordinates, player_facing)]}
             elif char == 's':
                 player_coordinates[0] -= 1
-                vars_vals = {"e": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0], "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 2 else [0], "i": [get_facing(level, player_coordinates, player_facing)]}
+                vars_vals = {"e": [1] if len(
+                    levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0],
+                             "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)][
+                                                 'actions']) >= 2 else [0],
+                             "i": [get_facing(level, player_coordinates, player_facing)]}
             elif char == 'd':
                 player_coordinates[1] += 1
-                vars_vals = {"e": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0], "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 2 else [0], "i": [get_facing(level, player_coordinates, player_facing)]}
+                vars_vals = {"e": [1] if len(
+                    levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0],
+                             "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)][
+                                                 'actions']) >= 2 else [0],
+                             "i": [get_facing(level, player_coordinates, player_facing)]}
             elif char == 'e':
-                player_coordinates = move_forward()
+                player_coordinates = move_forward(1, player_coordinates, player_facing)
+                vars_vals = {"e": [1] if len(
+                    levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0],
+                             "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)][
+                                                 'actions']) >= 2 else [0],
+                             "i": [get_facing(level, player_coordinates, player_facing)]}
             elif char == 'q':
-                ...
+                player_coordinates = move_forward(-1, player_coordinates, player_facing)
+                vars_vals = {"e": [1] if len(
+                    levels.blocks[get_facing(level, player_coordinates, player_facing)]['actions']) >= 1 else [0],
+                             "q": [1] if len(levels.blocks[get_facing(level, player_coordinates, player_facing)][
+                                                 'actions']) >= 2 else [0],
+                             "i": [get_facing(level, player_coordinates, player_facing)]}
             else:
-                if not re.search("(\\(.|\\[.)*.\?'.*'(\\)|\\])*", line) is None:
+                if not re.search("(\\(.|\\[.)*.\\?'.*'(\\)|\\])*", line) is None:
                     statement = re.sub('(\\(.)|(\\[.)|(\\])|(\\))', '', line).split('?')
                     vars_vals[statement[0]] = statement[1]
+
 
 main()
